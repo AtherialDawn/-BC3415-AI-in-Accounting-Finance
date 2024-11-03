7e73089bd23e8f2060607b762a38b0a33cac4d24
Peh Wee Din <111359182+AtherialDawn@users.noreply.github.com>
2024-11-03 22:33:04 +0800

    Add files via upload

---
+++ Homework 4.py
@@ -0,0 +1,34 @@
+from flask import Flask,render_template,request
+import google.generativeai as genai
+import os
+import markdown2
+import textblob
+
+api = "AIzaSyCEsQLEfb07F3TDcauwIjDoOZsZ79iAksM"
+genai.configure(api_key=api)
+client = genai.GenerativeModel('gemini-1.5-flash')
+
+app = Flask(__name__)
+
+@app.route("/", methods = ["GET","POST"])
+def index():
+    return(render_template("Homework 4.html"))
+
+@app.route("/ai_agent", methods = ["GET","POST"])
+def ai_agent():
+    return(render_template("Homework 4 AI Agent.html"))
+
+@app.route("/ai_agent_reply", methods=["GET", "POST"])
+def ai_agent_reply():
+    q = request.form.get("q")
+    response = client.generate_content(q)
+    # Convert the response text from Markdown to HTML
+    html_content = markdown2.markdown(response.text)
+    return render_template("Homework 4 AI Agent Reply.html", r = html_content)
+
+@app.route("/paynow", methods=["GET","POST"])
+def paynow():
+    return(render_template("Homework 4 Paynow DApp.html"))
+
+if __name__ == "__main__":
+    app.run()
